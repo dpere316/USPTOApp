d724e5f0cdb9ec998b0d74bba9702b770e32cd15
Daniel Perz <dpere316@fiu.edu>
2021-01-18 19:05:50 -0500

    Changed Classes to stateless components

--- frontend/src/components/Dashboard/components/DashboardNavigation.jsx
+++ frontend/src/components/Dashboard/components/DashboardNavigation.jsx
@@ -1,40 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function dashboardnavigation(props) {
+const DashboardNavigation = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    
-      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          DashBoard V3
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" to="/Dashboard/ViewUser">
+                View Users <span className="sr-only">(current)</span>{" "}
+              </Link>
+            </li>
 
-        DashBoard V3
+            {/* <li className="nav-item">
+          <Link className="nav-link" to="/Dashboard/AddUser">
+            Add User 
+          </Link>
+        </li>
 
-        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-         
-          <li className="nav-item">
-            <Link className="nav-link" to="/Dashboard/ViewUser">
-              View Users <span className="sr-only">(current)</span>{" "}
-            </Link>
-          </li>
-
-          {/* <li className="nav-item">
-            <Link className="nav-link" to="/Dashboard/AddUser">
-              Add User 
-            </Link>
-          </li>
-
-          <li className="nav-item">
-            <Link className="nav-link" to="/Dashboard/AddPatent">
-              Add Patent
-            </Link>
-          </li> */}
-
-        </ul>
-        
-      </div>
-    </nav>
-  
+        <li className="nav-item">
+          <Link className="nav-link" to="/Dashboard/AddPatent">
+            Add Patent
+          </Link>
+        </li> */}
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
-}
+};
 
-export default dashboardnavigation;
+export default DashboardNavigation;
