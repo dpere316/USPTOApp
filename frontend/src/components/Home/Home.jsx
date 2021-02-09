b973e2fb61e9c34dd40911b7e9b4b1c43ff1c766
amolinarossi <amolinarossi@gmail.com>
2021-02-09 09:54:38 -0500

    adding css for home page
    changes on app.css that add css to home.jsx. also added background image files in different variations. will delete when i determine which is the best.

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,25 +1,23 @@
 import React from "react";
-import Logo from "./backlines.svg";
+
+import "../../App.css"
+
 
 const Home = (props) => {
 
-    return (
-        
-   
-
-
-        <div         class="bg_image"
-        style={{
-          backgroundImage: 'url('+Logo+')',
-          backgroundSize: "cover",
-          height: "100vh",
-          color: "#f5f5f5"
-        }}
-      >
-           
-        </div>
+  return (
+    <body>    
       
-    );
+    <div className = "home-background" >
+      <div className = "home-page-title">
+      Patentify
+      </div>
+    </div>
+    
+    </body>
+
+    
+  );
 }
 
 export default Home;
