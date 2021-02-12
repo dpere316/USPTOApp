4d1294d06457cf159bdb1804c6b188e7de678b6c
amolinarossi <amolinarossi@gmail.com>
2021-02-12 12:52:54 -0500

    updating background..

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,27 +1,24 @@
 import React from "react";
-import { Link} from "react-router-dom";
-import "../../App.css"
-
+import {Link} from "react-router-dom";
 
 
 const Home = (props) => {
 
   return (
     <html>
-      <body className = "home-background" >    
+      <body class = "home-background" >    
         <div className = "welcome-text" >
           <p className = "home-page-description"> Welcome to</p>
           <p className = "home-page-title">Patentify</p>
           <p className = "home-page-description">AI assisted patent labeling</p>
         </div>
         <div className = "home-links">
-        
-        <Link to="/login" className="btn-home btn-primary">
-          Login     
-        </Link>
-        <Link to="/signup" className = "btn-home btn-primary">
-          SignUp
-        </Link>
+          <Link to="/login" className="btn-home btn-primary">
+            Login     
+          </Link>
+          <Link to="/signup" className = "btn-home btn-primary">
+            SignUp
+          </Link>
         </div>
       </body>
     </html>
