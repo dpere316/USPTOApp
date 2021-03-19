6a0caf1ff09c0c97c14ae350ca03d3d4cdd2a561
Daniel Perz <dpere316@fiu.edu>
2021-03-19 19:32:48 -0400

    Form Changed to Radio Buttons

--- frontend/src/components/PatentPage/PatentForm.jsx
+++ frontend/src/components/PatentPage/PatentForm.jsx
@@ -1,119 +1,144 @@
 import React from "react";
-import { Button, Form} from "react-bootstrap";
-import {useForm} from 'react-hook-form';
-import axios from 'axios';
+import { Button, Form, FormCheck} from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+
 
 const PatentForm = (props) => {
-
   const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
     // This is using axios to make a post request to our backend and send {name,email,password}
     // and store it in mongoDB
     axios({
-      url:"/patents/labels", // route in backend
-      method:"POST",
-      data:{
-        documentId:props.patents[0].documentId,
+      url: "/patents/labels", // route in backend
+      method: "POST",
+      data: {
+        documentId: props.patents[0].documentId,
         mal: data.mal,
-        hdw:data.hdw,
-        evo:data.evo,
-        spc:data.spc,
-        vis:data.vis,
-        nlp:data.nlp,
-        pln:data.pln,
-        kpr:data.kpr,
-      }
+        hdw: data.hdw,
+        evo: data.evo,
+        spc: data.spc,
+        vis: data.vis,
+        nlp: data.nlp,
+        pln: data.pln,
+        kpr: data.kpr,
+      },
     })
-    .then(response=>{
-      console.log("Data: ", response.data)
-      window.location.reload();
-    })
-    .catch(error => {
-      console.log("Error: ", error.data )
-    })
-  }
-  const nextPage = ()=>{
+      .then((response) => {
+        console.log("Data: ", response.data);
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log("Error: ", error.data);
+      });
+  };
+  const nextPage = () => {
     window.location.reload();
- }
+  };
 
   return (
     <div>
-
-      <Form className="container mt-5" method="POST" onSubmit={handleSubmit(onSubmit)}>
-        <Form.Group controlId="exampleForm.SelectCustomSizeSm">
-          <Form.Label>Is this a Machine Learning Patent?</Form.Label>
-          <Form.Control name ="mal" as="select" size="sm" ref={register} defaultValue="No">
-            <option>Yes</option>
-            <option>No</option>
-          </Form.Control>
+      <Form
+        className="container mt-5"
+        method="POST"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Form.Group>
+          <Form.Label>
+            Is this a Machine Learning Patent?
+          </Form.Label>
+          <div className='row-2'>
+          <FormCheck name="mal" inline type="radio" label="Yes" value='Yes' ref={register}/>
+          <FormCheck name="mal" inline type="radio" label="No"  value= 'No' ref={register} defaultChecked/>
+          </div>
         </Form.Group>
 
-        <Form.Group controlId="exampleForm.SelectCustomSizeSm">
-          <Form.Label>Is this a AI hardware Patent?</Form.Label>
-          <Form.Control name ="hdw" as="select" size="sm" ref={register} defaultValue="No">
-            <option>Yes</option>
-            <option >No</option>
-          </Form.Control>
+        <Form.Group>
+          <Form.Label>
+          Is this a AI hardware Patent?
+          </Form.Label>
+          <div className='row-2'>
+          <FormCheck  name="hdw" inline type="radio" label="Yes"  value='Yes' ref={register}/>
+          <FormCheck  name="hdw"inline type="radio" label="No"  value= 'No' ref={register} defaultChecked/>
+          </div>
+        </Form.Group>
+        
+        <Form.Group>
+          <Form.Label>
+          Is this a Evolutionary computation Patent?
+          </Form.Label>
+          <div className='row-2'>
+          <FormCheck  name="evo" inline type="radio" label="Yes" value='Yes' ref={register}/>
+          <FormCheck  name="evo" inline type="radio" label="No" value= 'No' ref={register} defaultChecked/>
+          </div>
         </Form.Group>
 
-        <Form.Group controlId="exampleForm.SelectCustomSizeSm">
-          <Form.Label>Is this a Evolutionary computation Patent?</Form.Label>
-          <Form.Control name ="evo" as="select" size="sm" ref={register} defaultValue="No">
-            <option>Yes</option>
-            <option >No</option>
-          </Form.Control>
+        <Form.Group>
+          <Form.Label>
+          Is this a Natural Language Processing Patent?
+          </Form.Label>
+          <div className='row-2'>
+          <FormCheck name="nlp" inline type="radio" label="Yes" value='Yes' ref={register}/>
+          <FormCheck name="nlp" inline type="radio" label="No"  value= 'No' ref={register} defaultChecked/>
+          </div>
         </Form.Group>
 
-        <Form.Group controlId="exampleForm.SelectCustomSizeSm">
-          <Form.Label>Is this a Natural Language Processing Patent?</Form.Label>
-          <Form.Control name ="nlp" as="select" size="sm" ref={register} defaultValue="No">
-            <option>Yes</option>
-            <option >No</option>
-          </Form.Control>
+        <Form.Group>
+          <Form.Label>
+          Is this a Speech Patent?
+          </Form.Label>
+          <div className='row-2'>
+          <FormCheck name="spc" inline type="radio" label="Yes" value='Yes' ref={register}/>
+          <FormCheck name="spc" inline type="radio" label="No"  value= 'No' ref={register} defaultChecked/>
+          </div>
         </Form.Group>
 
-        <Form.Group controlId="exampleForm.SelectCustomSizeSm">
-          <Form.Label>Is this a Speech Patent?</Form.Label>
-          <Form.Control name ="spc" as="select" size="sm" ref={register} defaultValue="No">
-            <option>Yes</option>
-            <option  >No</option>
-          </Form.Control>
+        <Form.Group>
+          <Form.Label>
+          Is this a Vision Patent?
+          </Form.Label>
+          <div className='row-2'>
+          <FormCheck name="vis" inline type="radio" label="Yes" value='Yes' ref={register}/>
+          <FormCheck name="vis" inline type="radio" label="No"  value= 'No' ref={register} defaultChecked/>
+          </div>
         </Form.Group>
 
-        <Form.Group controlId="exampleForm.SelectCustomSizeSm">
-          <Form.Label>Is this a Vision Patent?</Form.Label>
-          <Form.Control name ="vis" as="select" size="sm" ref={register} defaultValue="No">
-            <option>Yes</option>
-            <option>No</option>
-          </Form.Control>
+        <Form.Group>
+          <Form.Label>
+          Is this a Knowledge Processing Patent?
+          </Form.Label>
+          <div className='row-2'>
+          <FormCheck name="kpr" inline type="radio" label="Yes" value='Yes' ref={register}/>
+          <FormCheck name="kpr" inline type="radio" label="No"  value= 'No' ref={register} defaultChecked/>
+          </div>
         </Form.Group>
 
-        <Form.Group controlId="exampleForm.SelectCustomSizeSm">
-          <Form.Label>Is this a Knowledge Processing Patent?</Form.Label>
-          <Form.Control name ="kpr" as="select" size="sm" ref={register} defaultValue="No">
-            <option>Yes</option>
-            <option >No</option>
-          </Form.Control>
+        <Form.Group>
+          <Form.Label>
+          Is this a Planning/Control Patent?
+          </Form.Label>
+          <div className='row-2'>
+          <FormCheck  name="pln" inline type="radio" label="Yes" value='Yes' ref={register}/>
+          <FormCheck  name="pln" inline type="radio" label="No"  value= 'No' ref={register} defaultChecked/>
+          </div>
         </Form.Group>
 
-        <Form.Group controlId="exampleForm.SelectCustomSizeSm">
-          <Form.Label>Is this a Planning/Control Patent?</Form.Label>
-          <Form.Control name ="pln" as="select" size="sm" ref={register} defaultValue="No">
-            <option>Yes</option>
-            <option  >No</option>
-          </Form.Control>
-        </Form.Group>
-        <div className="row justify-content-between"> 
-        <Button type="submit" variant="primary" size="lg" className="col-3">
-          {" "}
-          Submit
-        </Button>
-        <Button variant="danger" size="lg" className="col-3" onClick={nextPage}>
+        <div className="row justify-content-around">
+          <Button type="submit" variant="primary" size="lg" className="col-3">
+            {" "}
+            Submit
+          </Button>
+          <Button
+            variant="danger"
+            size="lg"
+            className="col-3"
+            onClick={nextPage}
+          >
             {" "}
             Skip
           </Button>
-          </div>
+        </div>
       </Form>
     </div>
   );
